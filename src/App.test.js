94b222a90268bc35809cda060b7e0bd2052c44d1
aryan <ARYAN.YAGHOBI@GMAIL.COM>
2025-06-05 13:50:28 -0700

    Remove default test for custom landing page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,1 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-const linkElement = screen.getByText(/ci\/cd is working/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// No tests for this demo landing page yet.
